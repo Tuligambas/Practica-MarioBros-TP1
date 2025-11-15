e12e1b9137b9e26efb72103e8d9b4475604ded61
javdelag <javdelag@ucm.es>
2025-11-15 19:24:42 +0100

    Test 4 cambio showmessages por showerror

--- src/main/java/tp1/control/Commands/AddObjectCommand.java
+++ src/main/java/tp1/control/Commands/AddObjectCommand.java
@@ -31,7 +31,7 @@ public class AddObjectCommand extends AbstractCommand /* NoParamsCommand */ { //
         GameObject obj = game.parseObject(infoObj);
 
         if (obj == null) {
-            view.showMessage("Invalid game object: " + fullDescription);
+            view.showError("Invalid game object: " + fullDescription);
             return;
         }
 
